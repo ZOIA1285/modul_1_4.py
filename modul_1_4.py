fd2f51a703da5ddc81136da4502af8089154db86
ZOIA1285 <zoiamyshkova@gmail.com>
2024-10-19 20:41:50 +0300

    Add files via upload

---
+++ modul_1_4.py
@@ -0,0 +1,6 @@
+number_of_homework = 12
+number_of_hours  = 1.5
+сourse_name= 'Python'
+teme = number_of_hours / number_of_homework
+print('Курс: ' , сourse_name , ',' , 'всего задач: ' , number_of_homework , ',' , 'затрачено часов: ' ,
+      number_of_hours , ',' , 'среднее время выполнения: ' , teme , '.')
